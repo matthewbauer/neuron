c95fc9caf8afbf43c2d7cf5f9ffb78f05b421c26
yuche <i@yuche.me>
2020-09-15 01:01:41 +0800

    refactor(renderer): sign message should open hardware dialog

--- packages/neuron-ui/src/components/PasswordRequest/index.tsx
+++ packages/neuron-ui/src/components/PasswordRequest/index.tsx
@@ -181,7 +181,7 @@ const PasswordRequest = () => {
   }
 
   if (wallet.device) {
-    return <HardwareSign />
+    return <HardwareSign signType="transaction" history={history} wallet={wallet} onDissmiss={onDismiss} />
   }
 
   return (
